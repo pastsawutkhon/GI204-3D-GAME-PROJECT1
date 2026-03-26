258056e1e647d3d6861343843c9bbb638c9c7398
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-26 22:39:57 +0700

    add friction, change player move

--- Assets/Scripts/MagnusEffectKicks.cs
+++ Assets/Scripts/MagnusEffectKicks.cs
@@ -40,7 +40,7 @@ public class MagnusEffectKicks : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject, 0.2f);
+        Destroy(gameObject, 5f);
     }
 
     
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        float x = 0f;
-        
-        if (Input.GetKey(KeyCode.D))
-            x = 1f;
-        else if (Input.GetKey(KeyCode.A))
-            x = -1f;
+        float moveX = Input.GetAxis("Horizontal");
 
-        Vector3 move = new Vector3(x, 0f, 0f).normalized;
-        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveX * moveSpeed;
+
+        rb.linearVelocity = velocity;
+
+
+        
+        // float x = 0f;
+        
+        // if (Input.GetKey(KeyCode.D))
+        //     x = 1f;
+        // else if (Input.GetKey(KeyCode.A))
+        //     x = -1f;
+
+        // Vector3 move = new Vector3(x, 0f, 0f).normalized;
+        // transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
 
         Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x, -10f, 10f);
